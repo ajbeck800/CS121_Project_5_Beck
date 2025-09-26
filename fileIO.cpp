76b6c499fe648d21490ff34cba5991cb07fdd858
ajbeck800 <ajbeck800@gmail.com>
2025-09-26 18:38:48 +0000

    Completed fileIO.cpp

--- fileIO.cpp
+++ fileIO.cpp
@@ -1,5 +1,35 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string>
 
+int main(){
+	std::ifstream inFile;
+	std::string currentLine;
+	std::stringstream converter;
+	int numA, numB, sum;
+	std::string sNumA, sNumB;
+	std::string word;
+	
+	inFile.open("data.csv");
+	while (getline(inFile, currentLine)){
+		converter.clear();
+		converter.str(currentLine);
+				
+		getline(converter, sNumA, ',');
+		getline(converter, sNumB, ',');
+		getline(converter, word);
+		
+		converter.clear();
+		converter.str("");
+		converter << sNumA << " " << sNumB;
+                converter >> numA >> numB;
+		
+		sum = numA + numB;
+
+		for (int i = 0; i < sum; i++){
+			std::cout << word << " ";
+		} // end for
+		std::cout << std::endl;
+  } // end while
+  inFile.close();
+} // end main
